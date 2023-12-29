5137136e39435a2524a52ac9c7650fe0ebe9f52a
Mant1c0ra <154907040+Mant1c0ra@users.noreply.github.com>
2023-12-29 12:06:30 +0200

    Add files via upload

---
+++ My autobiography.py
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+    <title>My autobiography</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: rgb(164, 0, 158)240)
+            color: #333;
+        }
+
+        h1 {
+            color: #0066cc;
+        }
+
+        p {
+            font-size: 18px;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            margin-top: 20px;
+        }
+
+        table, th, td {
+            border: 1px solid #000000;
+        }
+
+        th, td {
+            padding: 15px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #b6cc11;
+        }
+
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        .important {
+            font-weight: bold;
+            color: #e60000;
+        }
+       
+    
+    </style>
+</head>
+
+<body>
+
+    <h1>My autobiography</h1>
+
+    <img src="C:\Users\progm\OneDrive\Рабочий стол\Фото\photo_2023-04-11_19-53-07.jpg" alt="Моя фотографія" style="width: 200px; border-radius: 10px;">
+
+    <p>Привіт! Мене звуть Олексій. Мені 19 років і це моя коротка автобіографія.</p>
+
+    <table>
+        <tr>
+            <th>Рік</th>
+            <th>Подія</th>
+        </tr>
+        <tr>
+            <td>2010-2019</td>
+            <td>Середня школа</td>
+        </tr>
+        <tr>
+            <td>2023-2025</td>
+            <td>Вища освіта (Український державний університет імені Михайла Драгоманова)</td>
+        </tr>
+    </table>
+
+    <p>Більше про мене можна дізнатися на моїй сторінці в
+        <a href="https://www.instagram.com/w1gl3/">соціальній мережі</a>.</p>
+
+    <p class="important">Дякую за Увагу!</p>
+
+
+</body>
+
+</html>
